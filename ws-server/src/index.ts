dffb4d8ec9cebed25bac22e1f484ab82a604a679
vinay bhat <bhatvinay74@gmail.com>
2025-07-28 21:43:00 +0530

    updated websocket server

--- ws-server/src/index.ts
+++ ws-server/src/index.ts
@@ -2,15 +2,17 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
-console.log("hii")
+console.log("hii");
 import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
-import { Kafka } from 'kafkajs';
+import { Kafka } from "kafkajs";
 
-const kafka = new Kafka({ clientId: 'chat-app', brokers: ['kafka1:9092', 'kafka2:9093','kafka3:9094'] });
+const kafka = new Kafka({
+  clientId: "chat-app",
+  brokers: ["kafka1:9092", "kafka2:9093", "kafka3:9094"],
+});
 const producer = kafka.producer();
-
 
 import { createClient } from "redis";
 const REDIS_URL = process.env.Redis_URL;
@@ -38,83 +40,89 @@ const subscriber = createClient({
   await subscriber.connect();
   await producer.connect();
   console.log("Connected to Redis");
-  console.log("connected to kafka server")
+  console.log("connected to kafka server");
 })();
 
 // // Handle potential errors for both clients
-// publisher.on('error', (err) => console.error('Publisher Redis Client Error:', err));
-// subscriber.on('error', (err) => console.error('Subscriber Redis Client Error:', err));
+publisher.on("error", (err) =>
+  console.error("Publisher Redis Client Error:", err)
+);
+subscriber.on("error", (err) =>
+  console.error("Subscriber Redis Client Error:", err)
+);
 // Create a WebSocket server on a specific port
-
 const userRoomMap = new Map<WebSocket, Map<string, number>>();
+const userSocketMap = new Map<number, WebSocket>();
 const rooms = new Map<string, Set<WebSocket>>();
 interface Message {
   userId?: number;
   roomId?: number;
   message?: string;
   subscribe?: boolean | null;
-  type:string
+  type: string;
+  join?: boolean | null;
+  priv_roomId_priv_type?: string | null;
 }
 
 wss.on("connection", (ws) => {
   console.log("user");
 
-
   ws.on("message", async (data: string) => {
     const message: Message = JSON.parse(data.toString());
-   
-    if (message.subscribe && message.roomId) {
-      const previousRoomId = userRoomMap.get(ws)?.keys().next().value!;
+    if (!userSocketMap.get(message.userId!)) {
+      userSocketMap.set(message.userId!, ws);
+    }
+    if (message.roomId && message.join!) {
+      const previousRoomId = userRoomMap
+        .get(ws)
+        ?.keys()
+        .next()
+        .value!.split("-")?.[0];
 
       //  Remove the socket from the previous room (if any)
-      if (
-        previousRoomId &&
-        rooms.has(previousRoomId) &&
-        previousRoomId !== `${message.roomId}-${message.type}`!
-      ) {
-        rooms.get(previousRoomId)!.delete(ws);
+      if (previousRoomId! != message.roomId.toString()!) {
+        rooms.get(previousRoomId!)!.delete(ws);
       }
 
       if (!rooms.has(`${message.roomId}-${message.type}`)) {
         rooms.set(`${message.roomId}-${message.type}`, new Set());
         await subscribeToRoomChannel(`${message.roomId}-${message.type}`); // Subscribe to this Redis room/channel only once
       }
+
       rooms.get(`${message.roomId}-${message.type}`)!.add(ws);
-      const userId = message.userId;
-      const roomMap = new Map<string, number>();
-      roomMap.set(`${message.roomId}-${message.type}`, userId!);
-      userRoomMap.set(ws, roomMap);
 
       if (message.message) {
         // const buffer: Buffer = Buffer.from(message.message);
-        await publishMessage(`${message.roomId}-${message.type}`, JSON.stringify(message));
+        await publishMessage(
+          `${message.roomId}-${message.type}`,
+          JSON.stringify(message)
+        );
       }
     }
-   else if(!message.subscribe){
-   const users = rooms.get(userRoomMap.get(ws)?.keys().next().value!);
-   ws.send("UNSUBSCRIBED FROM THE CHANNEL");
-    if (users?.size == 0) {
-      subscriber.unsubscribe(
-        userRoomMap.get(ws)?.keys().next().value!.toString()?.split("-")?.[0]!
-      );
-    }
-    users!.delete(ws);
-    userRoomMap.delete(ws);
+    //  else if(!message.subscribe){
+    //  const users = rooms.get(userRoomMap.get(ws)?.keys().next().value!);
+    //  ws.send("UNSUBSCRIBED FROM THE CHANNEL");
+    //   if (users?.size == 0) {
+    //     subscriber.unsubscribe(
+    //       userRoomMap.get(ws)?.keys().next().value!.toString()?.split("-")?.[0]!
+    //     );
+    //   }
+    //   users!.delete(ws);
+    //   userRoomMap.delete(ws);
 
-    }
-
+    //   }
   });
 
-  ws.on("close", () => {
-    const users = rooms.get(userRoomMap.get(ws)?.keys().next().value!);
-    if (users?.size == 0) {
-      subscriber.unsubscribe(
-        userRoomMap.get(ws)?.keys().next().value?.split("-")?.[0]!
-      );
+  ws.on("close", async () => {
+    const clients = rooms.get(userRoomMap.get(ws)!.keys().next().value!);
+    if (clients) {
+      clients.delete(ws);
+      if (clients.size === 0) {
+        rooms.delete(userRoomMap.get(ws)!.keys().next().value!);
+        subscriber.unsubscribe(userRoomMap.get(ws)!.keys().next().value!);
+      }
+      userRoomMap.delete(ws);
     }
-    ws.send("UNSUBSCRIBED FROM THE CHANNEL");
-    users!.delete(ws);
-    userRoomMap.delete(ws);
   });
 
   ws.on("error", (error) => {
@@ -122,59 +130,41 @@ wss.on("connection", (ws) => {
   });
 });
 
-console.log("WebSocket server started on port 8080");
-
 async function subscribeToRoomChannel(roomId: string) {
-  await subscriber.subscribe(
-    roomId.toString(),
-    async (messageStr: string, channel: string) => {
-      const clients = rooms.get(channel);
-    //   const user=clients?.values().next().value
-    //   if(user){
-      
+  try {
+    await subscriber.subscribe(
+      roomId.toString(),
+      async (messageStr: string, channel: string) => {
+        const clients = rooms.get(channel);
+        //   const user=clients?.values().next().value
+        //   if(user){
 
-    //   }
-    const [roomId,chatType]=channel.split("-")
-    console.log(chatType)
+        //   }
+        const [roomId, chatType] = channel.split("-");
 
-  if(chatType=="not_group"){
-    console.log("inside not group");
-      await producer.send({
-      topic: 'one-to-one-messages',
-      messages: [{ value: messageStr }],
-    });
-
-  }  
-  else if(chatType=="group"){
-      await producer.send({
-      topic: 'group-messages',
-      messages: [{ value:messageStr }],
-    });
-  }
-
-// For group
-      
-      if (clients) {
-        for (const client of clients) {
-          const roomMap = userRoomMap.get(client);
-          if (
-            client.readyState === WebSocket.OPEN &&
-            roomMap?.get(channel) != JSON.parse(messageStr).userId.toString()
-          ) {
-           
-            
-
-
-            client.send(messageStr);
-          }
+        if (chatType == "not_group") {
+          console.log("inside not group");
+          await producer.send({
+            topic: "one-to-one-messages",
+            messages: [{ value: messageStr }],
+          });
+        } else if (chatType == "group") {
+          await producer.send({
+            topic: "group-messages",
+            messages: [{ value: messageStr }],
+          });
         }
+
+        await SendMessageToUsers(clients, channel, messageStr);
       }
-    }
-  );
+    );
+  } catch (error: any) {
+    console.log(error.message);
+  }
 }
 
 // Example of publishing a message
-async function publishMessage(channel:string, message: string) {
+async function publishMessage(channel: string, message: string) {
   try {
     if (message) {
       console.log("inside pub");
@@ -186,8 +176,28 @@ async function publishMessage(channel:string, message: string) {
   }
 }
 
-
 app.listen(8080, () => {
   console.log("WebSocket server is running on port 8080");
+});
+
+async function SendMessageToUsers(
+  clients: Set<WebSocket> | undefined,
+  channel: string,
+  messageStr: string
+) {
+  try {
+    if (clients) {
+      for (const client of clients) {
+        const roomMap = userRoomMap.get(client)!;
+        if (
+          client.readyState === WebSocket.OPEN &&
+          roomMap.get(channel) != JSON.parse(messageStr).userId
+        ) {
+          client.send(messageStr);
+        }
+      }
+    }
+  } catch (error: any) {
+    console.log(error.message);
+  }
 }
-)
