66d185e65a664e37914b581b5e7659dbf4f2ab3c
vinay bhat <bhatvinay74@gmail.com>
2025-09-25 00:09:17 +0530

    update the folder name

--- reach_backend/utility/createRedisClient.py
+++
@@ -1,9 +0,0 @@
-import redis
-from app.core.config import settings
-redisClient =redis.Redis.from_url(url=f"redis://{settings.Redis_URL}:{int(settings.port)}",password=settings.password, decode_responses=True)
-
-def get_redis_client():
-    return redisClient
-
-
-
